dd6e5219b5d57aced660364940eb15c899a67aa9
polyanskiyit <polyanskiyit@gmail.com>
2017-02-12 19:21:40 +0200

    first

---
+++ Homework__1/AverageMark.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__1
+{
+    class AverageMark
+    {
+        public double result(double firstNum, double SecondNum, double ThirdNum)
+        {
+            //Console.WriteLine((firstNum + SecondNum + ThirdNum) / 3);
+            return ((firstNum + SecondNum + ThirdNum) / 3);
+
+        }
+    }
+}
---
+++ Homework__1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть першу оцiнку:");
+            double fn = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введiть другу оцiнку:");
+            double sn = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введiть третю оцiнку:");
+            double tn = Convert.ToDouble(Console.ReadLine());
+
+            AverageMark averageMark = new AverageMark();
+
+
+            Console.WriteLine("Середне число:");
+            Console.WriteLine(
+                averageMark.result(fn, sn, tn)
+                );
+
+            Console.ReadKey();
+        }
+    }
+}
